159cc20573560917da2b51bd45880e91c3d8c5fa
MYQ <jerry200223200223@126.com>
2024-03-07 14:52:44 +0800

    change preview port on 8080

--- src/components/about/NavBar.vue
+++ src/components/about/NavBar.vue
@@ -2,9 +2,15 @@
     <q-card flat style="background-color: transparent">
         <q-list dense class="row">
             <q-item
-                :class="`col-sm-${
+                :class="`col-md-${
                     12 / props.info_list.length
-                } col-xs-12 text-weight-bolder text-no-wrap`"
+                } col-xs-12 text-weight-bolder text-no-wrap column ${
+                    index === 0
+                        ? 'text-left'
+                        : index === props.info_list.length - 1
+                        ? 'text-right'
+                        : 'text-center'
+                }`"
                 v-for="(item, index) in props.info_list"
                 :key="index"
             >
